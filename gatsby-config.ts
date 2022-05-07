93a7fb7a15f2c76e12ca618c61a85ff35aa2c136
Marcin Bednarz <94953520+Yerbaneyro@users.noreply.github.com>
2022-05-07 10:17:20 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `code2137`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
+
+export default config;
