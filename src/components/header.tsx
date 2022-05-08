fec444659867fa792a9242f9a5ec4bdd4f1eae73
Joanna Bach <joannakatarzynabach@gmail.com>
2022-05-08 09:37:45 +0200

    Add navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { Link } from 'gatsby';
+
 
 const Header = () => {
   return (
     <header>
-      <Link to="/">code 2137</Link>
-      <nav>
-        <ul>
-          <li>
-            <Link to="career">Kariera</Link>
-          </li>
-          <li>
-            <Link to="services">Usługi</Link>
-          </li>
-          <li>
-            <Link to="about">O nas</Link>
-          </li>
-        </ul>
-      </nav>
+      
     </header>
   );
 };
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from './header';
-import styles from  './layout.module.scss'
+import * as layoutStyles from  './layout.module.scss'
+import Nav from "./nav";
 
 const Layout = (props: { children: React.ReactChild | React.ReactChild[]}) => {
   return (
-    <div className={styles.container}>
+    <div className={layoutStyles.container}>
       <div>
+        <Nav />
         <Header />
         {props.children}
       </div>
