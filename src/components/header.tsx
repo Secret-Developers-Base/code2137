024d8214235213a60af395c838a73223301110e5
Joanna Bach <joannakatarzynabach@gmail.com>
2022-05-09 18:16:21 +0200

    header problem

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ import '@easyfonts/league-spartan';
 const Header = () => {
   return (
     <header className={styles.container}>
-      <h1 className={styles.headerDesc}>Body Leasing <br /> <hr color='1b2a4e' />Recruitment Services <br /> <hr color='1b2a4e'/> IT Outsourcing</h1>
+      <h1 className={styles.headerDesc}>Body Leasing <br /> <hr color='1b2a4e' />Recruitment Services <br /> <hr color='1b2a4e' /> IT Outsourcing</h1>
       <div className={styles.circle}></div>
       <div className={styles.circle2}></div>
       <img className={styles.image} src={headerImage} />
