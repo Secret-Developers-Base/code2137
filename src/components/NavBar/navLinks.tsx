f755f96b85b60d3bd2608339140fbfb3114a4e8b
Bartosz Malecki <bartoszmalecki90@gmail.com>
2022-05-11 10:01:19 +0200

    added pages

--- src/components/NavBar/navLinks.tsx
+++ src/components/NavBar/navLinks.tsx
@@ -1,18 +1,45 @@
 import React from "react";
 import { Link } from "gatsby";
-import * as navStyles from './nav.module.scss';
+import * as navStyles from "./nav.module.scss";
 
 const NavLinks = (props: any) => {
   return (
     <ul className={navStyles.menu}>
-      <li className={navStyles.list} onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <Link className={navStyles.link} activeClassName={navStyles.activeLink} to="career">Kariera</Link>
+      <li
+        className={navStyles.list}
+        onClick={() => props.isMobile && props.closeMobileMenu()}
+      >
+        <Link
+          className={navStyles.link}
+          activeClassName={navStyles.activeLink}
+          to="/career"
+        >
+          Kariera
+        </Link>
       </li>
-      <li className={navStyles.list} onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <Link className={navStyles.link} activeClassName={navStyles.activeLink} to="services">Usługi</Link>
+      <li
+        className={navStyles.list}
+        onClick={() => props.isMobile && props.closeMobileMenu()}
+      >
+        <Link
+          className={navStyles.link}
+          activeClassName={navStyles.activeLink}
+          to="/services"
+        >
+          Usługi
+        </Link>
       </li>
-      <li className={navStyles.list} onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <Link className={navStyles.link} activeClassName={navStyles.activeLink} to="about">O nas</Link>
+      <li
+        className={navStyles.list}
+        onClick={() => props.isMobile && props.closeMobileMenu()}
+      >
+        <Link
+          className={navStyles.link}
+          activeClassName={navStyles.activeLink}
+          to="/about"
+        >
+          O nas
+        </Link>
       </li>
     </ul>
   );
---
+++ src/pages/services.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+//Components
+import Nav from "../components/organisms/NavBar/nav";
+//fonts
+import "@fontsource/roboto";
+//styles
+import "../styles/main.scss";
+
+const ServicesPage = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default ServicesPage;
