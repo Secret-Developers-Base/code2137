9f2525e74bb065eae50a82cd3404f932af47199b
Bartosz Malecki <bartoszmalecki90@gmail.com>
2022-05-11 12:31:11 +0200

    footer template started

--- src/components/templates/layout/layout.tsx
+++ src/components/templates/layout/layout.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import Header from "../../organisms/header/header";
 import * as layoutStyles from "./layout.module.scss";
 import Nav from "../../organisms/NavBar/nav";
+import Footer from "../footer/footer";
 
 const Layout = (props: { children: React.ReactChild | React.ReactChild[] }) => {
-    return (
-        <div className={layoutStyles.container}>
-            <div>
-                <Nav />
-                <Header />
-                {props.children}
-            </div>
-        </div>
-    );
+  return (
+    <div className={layoutStyles.container}>
+      <div>
+        <Nav />
+        <Header />
+        {props.children}
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
