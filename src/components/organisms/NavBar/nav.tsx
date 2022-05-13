291e03f5e804a1bec856d8e65e4ebe2bfa784acf
Marcin Bednarz <94953520+Yerbaneyro@users.noreply.github.com>
2022-05-13 21:11:46 +0200

    remove imports

--- src/components/organisms/NavBar/nav.tsx
+++ src/components/organisms/NavBar/nav.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import "@fontsource/righteous";
-import "@fontsource/roboto";
+
 import MobileNavigation from "./mobileNavigation";
 import Navigation from "./navigation";
 
 const Nav = () => {
-
-  return (
-    <div>
-      <MobileNavigation />
-      <Navigation />
-    </div>
-  );
+    return (
+        <div>
+            <MobileNavigation />
+            <Navigation />
+        </div>
+    );
 };
 
 export default Nav;
--- src/pages/career.tsx
+++ src/pages/career.tsx
@@ -3,16 +3,16 @@ import * as React from "react";
 import Layout from "../components/templates/layout/layout";
 import CareersContent from "../components/templates/careers_content/careers_content";
 //fonts
-import "@fontsource/roboto";
+
 //styles
 import "../styles/main.scss";
 
 const CareerPage = () => {
-  return (
-    <Layout>
-      <CareersContent />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <CareersContent />
+        </Layout>
+    );
 };
 
 export default CareerPage;
