c9236cd677fb6475e7ba26ed98106a1665d04c24
Marcin Bednarz <94953520+Yerbaneyro@users.noreply.github.com>
2022-05-13 20:55:48 +0200

    remove fonts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,29 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `code2137`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-smoothscroll",
-    "gatsby-transformer-typescript-css-modules",
-    {
-      resolve: `gatsby-plugin-sass-resources`,
-      options: {
-        resources: ["./src/styles/SASSResourceLoader.scss"],
-      },
+    siteMetadata: {
+        title: `code2137`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-  ],
+    plugins: [
+        "gatsby-plugin-sass",
+        "gatsby-plugin-smoothscroll",
+        "gatsby-transformer-typescript-css-modules",
+        {
+            resolve: `gatsby-plugin-sass-resources`,
+            options: {
+                resources: ["./src/styles/SASSResourceLoader.scss"],
+            },
+        },
+        {
+            resolve: "gatsby-plugin-web-font-loader",
+            options: {
+                typekit: {
+                    id: process.env.TYPEKIT_ID,
+                },
+            },
+        },
+    ],
 };
 
 export default config;
