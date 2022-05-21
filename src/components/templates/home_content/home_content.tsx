47dc76e4229fdce4f789d85d1279d38473997926
Joanna Bach <joannakatarzynabach@gmail.com>
2022-05-21 12:50:05 +0200

    Add LP form

--- src/components/templates/home_content/home_content.tsx
+++ src/components/templates/home_content/home_content.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import WhatWeOfferSection from "../../molecules/what_we_offer_section/what_we_offer_section";
 import WhoWeAreSection from "../../molecules/who_we_are_section/who_we_are_section";
 import WhyUsSection from "../../molecules/why_us_section/why_us_section";
+import MainForm from "../../molecules/main_form/main_form";
 
 const HomeContent = () => {
     return (
@@ -10,6 +11,7 @@ const HomeContent = () => {
             <WhoWeAreSection />
             <WhatWeOfferSection />
             <WhyUsSection />
+            <MainForm />
         </>
     );
 };
