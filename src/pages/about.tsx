8a9aadc9d11efcdabddf7eec4d9d14a3c42a92ac
Bartosz Małecki <bartoszmalecki90@gmail.com>
2022-05-23 20:58:16 +0200

    asd

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 //Components
-import Footer from "../components/templates/footer/footer";
-import Nav from "../components/organisms/NavBar/nav";
+import Layout from "../components/templates/layout/layout";
 import AboutUsSection from "../components/templates/about_us_content/about_us_content";
 //fonts
 
@@ -11,9 +10,9 @@ import "../styles/main.scss";
 const AboutPage = () => {
   return (
     <div>
-      <Nav />
-      <AboutUsSection />
-      <Footer />
+      <Layout>
+        <AboutUsSection />
+      </Layout>
     </div>
   );
 };
