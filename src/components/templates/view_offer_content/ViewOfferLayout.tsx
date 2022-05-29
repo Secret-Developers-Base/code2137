d8ffa5463e73f2921a2f5a1988338f653b793463
Marcin Bednarz <94953520+Yerbaneyro@users.noreply.github.com>
2022-05-29 19:45:11 +0200

    changing layout of job offer

--- src/components/templates/view_offer_content/ViewOfferLayout.tsx
+++ src/components/templates/view_offer_content/ViewOfferLayout.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import * as layoutStyles from "./layout.module.scss";
 import Nav from "../../organisms/NavBar/nav";
+import Footer from "../../organisms/footer/footer";
 
 const Layout = (props: { children: React.ReactChild | React.ReactChild[] }) => {
-  return (
-    <div className={layoutStyles.container}>
-      <div>
-        <Nav />
-        {props.children}
-      </div>
-    </div>
-  );
+    return (
+        <div className={layoutStyles.container}>
+            <div>
+                <Nav />
+                {props.children}
+                <Footer />
+            </div>
+        </div>
+    );
 };
 
 export default Layout;
--- src/pages/oferty-pracy/[name].tsx
+++ src/pages/oferty-pracy/[name].tsx
@@ -1,58 +1,58 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ViewOfferNav from "../../components/molecules/view_offer/view_offer_nav/View_Offer_Nav";
-import ViewOfferLayout from "../../components/templates/view_offer_content/ViewOfferLayout";
+import Layout from "../../components/templates/layout/layout";
 import ViewOfferBody from "../../components/molecules/view_offer/view_offer_body/Body";
 import Content from "../../components/molecules/view_offer/view_offer_maincontent/Content";
+import * as styles from "./offer.module.scss";
 
 export default function viewOffer(params: { name: String }) {
-  const name = params.name;
+    const name = params.name;
 
-  const [offer, setOffer] = useState<any>();
-  const [loaded, setLoaded] = useState<boolean>(false);
+    const [offer, setOffer] = useState<any>();
+    const [loaded, setLoaded] = useState<boolean>(false);
 
-  const fetchOnLoad = async (name: String) => {
-    axios(`http://localhost:8080/api/getJob/${name}`)
-      .then((res) => {
-        if (res.data.success) {
-          setOffer(res.data.data.offerInfo);
-          setLoaded(true);
-        } else {
-          document.getElementById("loading")!.innerText = "Couldn't load the offer!"
-          setLoaded(true);
-        }
-      })
-      .catch((err) => {
-        window.location.href = "/";
-      });
-  };
+    const fetchOnLoad = async (name: String) => {
+        axios(`http://localhost:8080/api/getJob/${name}`)
+            .then((res) => {
+                if (res.data.success) {
+                    setOffer(res.data.data.offerInfo);
+                    setLoaded(true);
+                } else {
+                    document.getElementById("loading")!.innerText =
+                        "Couldn't load the offer!";
+                    setLoaded(true);
+                }
+            })
+            .catch((err) => {
+                window.location.href = "/";
+            });
+    };
 
-  useEffect(() => {
-    fetchOnLoad(name);
-  }, [name]);
+    useEffect(() => {
+        fetchOnLoad(name);
+    }, [name]);
 
-  return (
-    <div style={{ marginTop: "70px" }}>
-      <ViewOfferLayout>
-        {loaded ? (
-          <>
-            <ViewOfferNav
-              title={offer.title}
-              UploadData={offer.dateCreated}
-              location={offer.localisation}
-            />
-            <ViewOfferBody
-              usedTechnology={offer.usedTechnology}
-              whatAreWeLookingFor={offer.whatAreWeLookingFor}
-              monthlyPay={offer.monthlyPay}
-              experienceLvl={offer.experienceLvl}
-              mainContent={offer.mainContent}
-            />
-          </>
-        ) : (
-          <div id="loading">Loading...</div>
-        )}
-      </ViewOfferLayout>
-    </div>
-  );
+    return (
+        <Layout location="job_offer">
+            {loaded ? (
+                <div className={styles.offer_container}>
+                    <ViewOfferNav
+                        title={offer.title}
+                        UploadData={offer.dateCreated}
+                        location={offer.localisation}
+                    />
+                    <ViewOfferBody
+                        usedTechnology={offer.usedTechnology}
+                        whatAreWeLookingFor={offer.whatAreWeLookingFor}
+                        monthlyPay={offer.monthlyPay}
+                        experienceLvl={offer.experienceLvl}
+                        mainContent={offer.mainContent}
+                    />
+                </div>
+            ) : (
+                <div id="loading">Loading...</div>
+            )}
+        </Layout>
+    );
 }
